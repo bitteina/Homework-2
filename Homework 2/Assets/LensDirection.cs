ca21deb7e7dda0fc3ca95895521b3a9225ff78e0
bitteina <banskihanski@gmail.com>
2026-02-03 14:07:57 +0200

    Comments in LensDirection

--- Homework 2/Assets/LensDirection.cs
+++ Homework 2/Assets/LensDirection.cs
@@ -6,7 +6,7 @@ public class LensDirection : MonoBehaviour
     public Transform viewTarget;
     public Transform mainCamera;
     public Transform mag;
-    private Quaternion prevRotation;
+    //private Quaternion prevRotation;
 
     void Update()
     {
@@ -15,18 +15,17 @@ public class LensDirection : MonoBehaviour
         //temp.position = mainCamera.position.Inverse();
         //transform.rotation = transform.rotation * Quaternion.Inverse(prevRotation);
 
-        Quaternion deltaRot = transform.rotation * Quaternion.Inverse(prevRotation);
+        //Quaternion deltaRot = transform.rotation * Quaternion.Inverse(prevRotation);
         //transform.localRotation = Quaternion.Inverse(mag.localRotation);// * transform.localRotation; //prevRotation
 
-        transform.rotation =  transform.rotation * deltaRot;
+        //transform.rotation =  transform.rotation * deltaRot;
 
         viewTarget.position = (transform.position - mainCamera.position).normalized * 8.0f + transform.position;
         transform.LookAt(viewTarget, mainCamera.up);
-      
 
         //transform.LookAt(mainCamera, mainCamera.up);
         //transform.Rotate(0f,180f,0f);
-        prevRotation = mag.rotation;
+        //prevRotation = mag.rotation;
     }
     
 }
