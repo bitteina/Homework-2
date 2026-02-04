12bd5c1cf32da98e0c76827cad663ca0ae23a239
bitteina <banskihanski@gmail.com>
2026-02-04 10:35:00 +0200

    2nd build

--- Homework 2/Assets/LensDirection.cs
+++ Homework 2/Assets/LensDirection.cs
@@ -10,21 +10,8 @@ public class LensDirection : MonoBehaviour
 
     void Update()
     {
-        //transform.rotation = mag.rotation;
-        //var temp = mainCamera;
-        //temp.position = mainCamera.position.Inverse();
-        //transform.rotation = transform.rotation * Quaternion.Inverse(prevRotation);
-
-        //Quaternion deltaRot = transform.rotation * Quaternion.Inverse(prevRotation);
-        //transform.localRotation = Quaternion.Inverse(mag.localRotation);// * transform.localRotation; //prevRotation
-
-        //transform.rotation =  transform.rotation * deltaRot;
-
         viewTarget.position = (transform.position - mainCamera.position).normalized * 8.0f + transform.position;
         transform.LookAt(viewTarget, mag.up);
-        //transform.LookAt(mainCamera, mainCamera.up);
-        //transform.Rotate(0f,180f,0f);
-        //prevRotation = mag.rotation;
     }
     
 }
